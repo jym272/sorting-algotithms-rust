7a54cd48af1a036e5fbf676cbc853014575e69ce
jorge <jym272@gmail.com>
2022-05-29 20:32:39 -0300

    feat: trait and function writed

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,40 @@
+//Sort stable: equal values doesn't move
+// mod bubblesort;
+
+trait Sorter {
+    fn sort<T>(slice: &mut [T])
+    where
+        T: Ord;
+}
+
+fn sort<T, S>(slice: &mut [T])
+where
+    T: Ord,
+    S: Sorter,
+{
+    S::sort(slice);
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    struct StdSorter;
+    impl Sorter for StdSorter{
+        fn sort<T>(slice: &mut [T])
+        where
+            T: Ord,
+        {
+            slice.sort();
+        }
+    }
+}
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn std_works() {
+        let mut v = vec![2, 1, 3, 1];
+        sort::<i32, StdSorter>(&mut v);
+        assert_eq!(v, vec![1, 1, 2, 3]);
     }
 }
